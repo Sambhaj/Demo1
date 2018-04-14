a4285b87c60b6bf824b5409a1f6cac0b0c277755
Sambhaji <sambhajiwagh56@gmail.com>
2018-04-14 12:11:16 +0530

    updated

---
+++ uvadvisor/new 4.php
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+$(document).ready(function () {
+
+function onchange() {
+var box1 = document.getElementById('field_1');
+var box2 = document.getElementById('field_2');
+box2.value = box1.value;
+}
+});
+</script>
